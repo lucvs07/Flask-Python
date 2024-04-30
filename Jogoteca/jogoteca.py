cde2f9936c82d70bac04ee132d43bccaab6cca00
Lucas Grecco <lucas.rgrecco@gmail.com>
2024-04-30 14:52:24 -0300

    Implementando Login e Logout

--- Jogoteca/jogoteca.py
+++ Jogoteca/jogoteca.py
@@ -1,10 +1,11 @@
 # Importando o framework Flask
-from flask import Flask, render_template, request, redirect # type: ignore
+from flask import Flask, render_template, request, redirect, session, flash # type: ignore
 # Importar classe jogo
 from modelos.jogo import Jogo
 
 # Criando a aplicação
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('Valorant', 'FPS', 'PC')
@@ -27,6 +28,26 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
-    
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(f"Usuário: {session['usuario_logado']} logado com sucesso")
+        return redirect('/')
+    else:
+        flash('Usuário não logado')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash(f'Logout efetuado com sucesso !')
+    return redirect('/')
+            
 # Rodar a aplicação
 app.run(debug=True)
